1a8a556c19f3d6cdfcd111901497072e03c73bc0
Reno-mathews <renomathews10pro@gmail.com>
2025-12-15 07:35:40 +0530

    Update

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,6 +4,14 @@ function Header(){
     return(
         <header>
             <h1>My website</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
         </header>
     );
 }
