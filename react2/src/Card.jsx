9699cc942e73b899bac4184ff6d6a81d315813e3
Reno-mathews <renomathews10pro@gmail.com>
2025-12-16 14:13:26 +0530

    Update

--- react2/src/Card.jsx
+++ react2/src/Card.jsx
@@ -2,9 +2,9 @@ import profilepic from './assets/download.jpg'
 function Card(){
     return (
         <div className="card">
-            <img src={profilepic} alt="profile picture"></img>
-            <h2>Bro Code</h2>
-            <p>sdfsdfsdfsf</p>
+            <img className="card-image" src={profilepic} alt="profile picture"></img>
+            <h2 className="card-title">Bro Code</h2>
+            <p className="card-text">sdfsdfsdfsf</p>
 
 
         </div>
