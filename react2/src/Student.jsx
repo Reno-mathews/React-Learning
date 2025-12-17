e7070b3d18e811c78ec5279064ecc04239508635
Reno-mathews <renomathews10pro@gmail.com>
2025-12-17 18:29:34 +0530

    Update

--- react2/src/Student.jsx
+++ react2/src/Student.jsx
@@ -3,6 +3,9 @@ function Student(props){
     return(
         <div>
             <p>Name: {props.name}</p>
+            <p>Age: {props.age}</p>
+            <p>Student: {props.isStudent}</p>
+            <p></p>
         </div>
     );
 }
