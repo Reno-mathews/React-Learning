8170011661e834cd971fffbe709d3648b4cee0d4
Reno-mathews <renomathews10pro@gmail.com>
2025-12-17 20:59:15 +0530

    Update

--- react2/src/Student.jsx
+++ react2/src/Student.jsx
@@ -1,10 +1,10 @@
 
 function Student(props){
     return(
-        <div>
+        <div className="Student">
             <p>Name: {props.name}</p>
             <p>Age: {props.age}</p>
-            <p>Student: {props.isStudent}</p>
+            <p>Student: {props.isStudent ? "Yes" : "No"}</p>
             <p></p>
         </div>
     );
